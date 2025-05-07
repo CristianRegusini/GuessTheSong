6007ff04b282c0e8f45e33d6fcaa4b6cfcf2ce67
MassimilianoMazzeo <mazzeo.massimiliano.mm@gmail.com>
2025-05-07 16:30:49 +0200

    Update Client.py
    client funzionante

--- Client.py
+++ Client.py
@@ -9,31 +9,45 @@ def main():
         client_socket.connect((host, port))  # Connessione al server
         print(f"Connesso a {host}:{port}")
 
+
+
         while True:
+
             # Riceve la strofa dal server
             strofa = client_socket.recv(1024).decode('utf-8')
             print(strofa)
 
-            # Riceve la domanda per l'artista
+            # Risposta titolo
+            title_question = client_socket.recv(1024).decode('utf-8')
+            print(title_question)
+            #title = input("inserisci il titolo")
+            title = input()
+
+            #invia la risposta
+            client_socket.sendall(title.encode('utf-8'))
+
+            # Riceve la richiesta per l'artista
             artist_question = client_socket.recv(1024).decode('utf-8')
             print(artist_question)
-            artista = input("Rispondi all'artista: ")
+            #artista = input("Inserire nome dell'artista: ")
+            artista = input()
 
             # Invia la risposta dell'artista
             client_socket.sendall(artista.encode('utf-8'))
 
-            # Riceve la domanda per l'anno
+            # Riceve la richiesta per l'anno
             year_question = client_socket.recv(1024).decode('utf-8')
             print(year_question)
-            anno = input("Rispondi all'anno: ")
+            #anno = input("Inserire anno : ")
+            anno = input()
 
             # Invia la risposta dell'anno
             client_socket.sendall(anno.encode('utf-8'))
 
-            # Riceve la domanda per il featuring
+            # Riceve la richiesta per il featuring
             featuring_question = client_socket.recv(1024).decode('utf-8')
             print(featuring_question)
-            featuring = input("Rispondi al featuring: ")
+            featuring = input()
 
             # Invia la risposta del featuring
             client_socket.sendall(featuring.encode('utf-8'))
@@ -45,15 +59,13 @@ def main():
             # Chiede se il giocatore vuole continuare
             continue_game = client_socket.recv(1024).decode('utf-8')
             print(continue_game)
-            risposta = input("Vuoi continuare a giocare? (si/no): ")
-
+            risposta = input("vuoi continuare? si/no")
             # Invia la risposta per continuare
             client_socket.sendall(risposta.encode('utf-8'))
 
-            if risposta.lower() != 'si':
+            if risposta.lower() == 'no':
+                print("Gioco terminato.")
                 break
-
-        print("Gioco terminato.")
 
     except Exception as e:
         print(f"Errore: {e}")  # In caso di errore, lo stampa
